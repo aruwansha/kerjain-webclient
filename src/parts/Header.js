474579599e4ad6ac74c4992dded432efce7e3645
aruwansha <ilhamfebrianarwansyah@gmail.com>
2021-06-05 17:38:40 +0700

    adding animation using react-reveal fade

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -4,48 +4,52 @@ import Button from "elements/Button";
 
 import BrandIcon from "parts/BrandIcon";
 
+import Fade from "react-reveal/Fade";
+
 export default function Header(props) {
-    const getNavLinkClass = (path) => {
-        return props.location.pathname === path ? " active" : "";
-      };
+  const getNavLinkClass = (path) => {
+    return props.location.pathname === path ? " active" : "";
+  };
   return (
-    <header className="spacing-sm">
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light">
-          <BrandIcon style={{marginRight: 30, fontSize: 32}} />
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav">
-              <li className={`nav-item${getNavLinkClass("/")}`}>
-                <Button className="nav-link" type="link" href="/">
-                  Beranda
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/kategori")}`}>
-                <Button className="nav-link" type="link" href="/kategori">
-                  Kategori
-                </Button>
-              </li>
-            </ul>
-            <ul className="navbar-nav ml-auto">
-              <li className={`nav-item`}>
-                <Button className="nav-link" type="link" href="/" isExternal >
-                  Jadi Mitra Kami
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/login")}`}>
-                <Button className="nav-link" type="link" href="/login">
-                  Login
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/register")}`}>
-                <Button className="nav-link" type="link" href="/register">
-                  Register
-                </Button>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </header>
+    <Fade>
+      <header className="spacing-sm">
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <BrandIcon style={{ marginRight: 30, fontSize: 32 }} />
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav">
+                <li className={`nav-item${getNavLinkClass("/")}`}>
+                  <Button className="nav-link" type="link" href="/">
+                    Beranda
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/kategori")}`}>
+                  <Button className="nav-link" type="link" href="/kategori">
+                    Kategori
+                  </Button>
+                </li>
+              </ul>
+              <ul className="navbar-nav ml-auto">
+                <li className={`nav-item`}>
+                  <Button className="nav-link" type="link" href="/" isExternal>
+                    Jadi Mitra Kami
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/login")}`}>
+                  <Button className="nav-link" type="link" href="/login">
+                    Login
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/register")}`}>
+                  <Button className="nav-link" type="link" href="/register">
+                    Register
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </Fade>
   );
 }
--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -4,67 +4,80 @@ import Star from "assets/images/icons/star.svg";
 
 import Button from "elements/Button";
 
+import Fade from "react-reveal/Fade";
+
 export default function MostPicked(props) {
   return (
     <section id="mostPicked" className="container">
-      <h4 className="mb-3">Paling Laris</h4>
-      <div className="container-grid">
-        {props.data.map((freelancer, index) => {
-          return (
-            <div className={`item column-3 row-1`} key={`mostPicked-${index}`}>
-              <div className="card card-featured">
-                <figure className="img-wrapper" style={{ height: 150 }}>
-                  <img
-                    src={freelancer.imgUrl}
-                    alt={freelancer.title}
-                    className="img-cover"
-                  />
-                </figure>
-                <div className="meta-wrapper">
-                  <div className="row">
-                    <div className="col-2">
-                      <figure className="img-rounder">
-                        <img
-                          src={freelancer.userId.imgUrl}
-                          alt="profile"
-                          className="profile-pic"
-                        />
-                      </figure>
-                    </div>
-                    <div className="col">
-                      <p
-                        className="text-gray-800"
-                        style={{ margin: 0, fontSize: 14 }}
-                      >{`${freelancer.userId.name.split(" ")[0]} ${
-                        freelancer.userId.name.split(" ")[1]
-                      }`}</p>
-                      <p
-                        className="text-gray-600"
-                        style={{ margin: 0, fontSize: 12 }}
+      <Fade bottom>
+        <h4 className="mb-3">Paling Laris</h4>
+        <div className="container-grid">
+          {props.data.map((freelancer, index) => {
+            return (
+              <Fade bottom delay={500 * index}>
+                <div
+                  className={`item column-3 row-1`}
+                  key={`mostPicked-${index}`}
+                >
+                  <div className="card card-featured">
+                    <figure className="img-wrapper" style={{ height: 150 }}>
+                      <img
+                        src={freelancer.imgUrl}
+                        alt={freelancer.title}
+                        className="img-cover"
+                      />
+                    </figure>
+                    <div className="meta-wrapper">
+                      <div className="row">
+                        <div className="col-2">
+                          <figure className="img-rounder">
+                            <img
+                              src={freelancer.userId.imgUrl}
+                              alt="profile"
+                              className="profile-pic"
+                            />
+                          </figure>
+                        </div>
+                        <div className="col">
+                          <p
+                            className="text-gray-800"
+                            style={{ margin: 0, fontSize: 14 }}
+                          >{`${freelancer.userId.name.split(" ")[0]} ${
+                            freelancer.userId.name.split(" ")[1]
+                          }`}</p>
+                          <p
+                            className="text-gray-600"
+                            style={{ margin: 0, fontSize: 12 }}
+                          >
+                            <img
+                              src={Star}
+                              alt="icon star"
+                              style={{ width: 12 }}
+                            />{" "}
+                            {freelancer.rating}
+                          </p>
+                        </div>
+                      </div>
+                      <Button
+                        type="link"
+                        className="stretched-link d-block text-gray-800"
+                        href={`/freelancer/${freelancer._id}`}
                       >
-                        <img src={Star} alt="icon star" style={{ width: 12 }} />{" "}
-                        {freelancer.rating}
-                      </p>
+                        <h6>{freelancer.title}</h6>
+                      </Button>
                     </div>
+                    <Button
+                      type="link"
+                      className="stretched-link"
+                      href={`/freelancer/${freelancer._id}`}
+                    ></Button>
                   </div>
-                  <Button
-                    type="link"
-                    className="stretched-link d-block text-gray-800"
-                    href={`/freelancer/${freelancer._id}`}
-                  >
-                    <h6>{freelancer.title}</h6>
-                  </Button>
                 </div>
-                <Button
-                  type="link"
-                  className="stretched-link"
-                  href={`/freelancer/${freelancer._id}`}
-                ></Button>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+              </Fade>
+            );
+          })}
+        </div>
+      </Fade>
     </section>
   );
 }
